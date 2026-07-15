329a3debb721ae67d136bb1999b0909673de00cd
SadSmileXD <phycoeco@naver.com>
2026-07-15 15:08:26 +0900

    Add files via upload

---
+++ SubscribeManager.cs
@@ -0,0 +1,140 @@
+﻿using System;
+using System.Collections.Generic;
+using UnityEngine;
+
+public enum SubscribeType
+{
+    None,
+  
+
+}
+public class SubscribeManager : MonoBehaviour
+{
+    public static SubscribeManager instance { get; private set; }
+
+    private Dictionary<SubscribeType, Delegate> subscriptions = new Dictionary<SubscribeType, Delegate>();
+
+    private void Awake()
+    {
+        if (instance == null)
+        {
+            instance = this;
+            DontDestroyOnLoad(gameObject);
+            return;
+        }
+        else
+        {
+            Destroy(this.gameObject);
+            return;
+        }
+    }
+
+    #region 인자가 없는 구독 (Action)
+    /// <summary>
+    /// 매개변수가 없는 이벤트를 구독합니다.
+    /// </summary>
+    /// <param name="type">구독할 이벤트의 종류</param>
+    /// <param name="action">이벤트 발생 시 실행할 메서드</param>
+    public void Subscribe(SubscribeType type, Action action)
+    {
+        if (subscriptions.ContainsKey(type))
+            subscriptions[type] = (Action)subscriptions[type] + action;
+        else
+            subscriptions[type] = action;
+
+        Debug.Log($"Subscribed to {type}");
+    }
+
+    /// <summary>
+    /// 등록된 매개변수가 없는 이벤트의 구독을 해제합니다.
+    /// </summary>
+    /// <param name="type">해제할 이벤트의 종류</param>
+    /// <param name="action">해제할 메서드</param>
+    public void Unsubscribe(SubscribeType type, Action action)
+    {
+        if (subscriptions.ContainsKey(type))
+        {
+            subscriptions[type] = (Action)subscriptions[type] - action;
+            if (subscriptions[type] == null)
+                subscriptions.Remove(type);
+        }
+    }
+
+    /// <summary>
+    /// 해당 이벤트를 발생시켜 구독 중인 모든 메서드를 호출합니다.
+    /// </summary>
+    /// <param name="type">발행할 이벤트의 종류</param>
+    public void Publish(SubscribeType type)
+    {
+        if (subscriptions.TryGetValue(type, out var del))
+        {
+            if (del is Action action)
+                action.Invoke();
+            else
+                Debug.LogError($"Type mismatch for {type}");
+        }
+    }
+    #endregion
+
+    #region 인자가 있는 구독 (Action<T>)
+    /// <summary>
+    /// 데이터(T)를 전달받는 이벤트를 구독합니다.
+    /// </summary>
+    /// <typeparam name="T">전달받을 데이터의 타입</typeparam>
+    /// <param name="type">구독할 이벤트의 종류</param>
+    /// <param name="action">이벤트 발생 시 데이터를 받아
+    /// 실행할 메서드</param>
+    public void Subscribe<T>(SubscribeType type, Action<T> action)
+    {
+        if (subscriptions.ContainsKey(type))
+        {
+            // 잘못된 타입 캐스팅 방지를 위한 안전 장치
+            if (subscriptions[type] is Action<T> existing)
+                subscriptions[type] = existing + action;
+            else
+                Debug.LogError($"Cast failed for {type}");
+        }
+        else
+        {
+            subscriptions[type] = action;
+        }
+    }
+
+    /// <summary>
+    /// 등록된 데이터(T) 기반 이벤트의 구독을 해제합니다.
+    /// </summary>
+    /// <typeparam name="T">전달받던 데이터의 타입</typeparam>
+    /// <param name="type">해제할 이벤트의 종류</param>
+    /// <param name="action">해제할 메서드</param>
+    public void Unsubscribe<T>(SubscribeType type, Action<T> action)
+    {
+        if (subscriptions.ContainsKey(type))
+        {
+            if (subscriptions[type] is Action<T> existing)
+            {
+                subscriptions[type] = existing - action;
+                if (subscriptions[type] == null)
+                    subscriptions.Remove(type);
+            }
+        }
+    }
+
+    /// <summary>
+    /// 데이터를 포함하여 이벤트를 발생시키고,
+    /// 구독 중인 모든 메서드에 데이터를 전달합니다.
+    /// </summary>
+    /// <typeparam name="T">전달할 데이터의 타입</typeparam>
+    /// <param name="type">발행할 이벤트의 종류</param>
+    /// <param name="data">구독자들에게 보낼 데이터</param>
+    public void Publish<T>(SubscribeType type, T data)
+    {
+        if (subscriptions.TryGetValue(type, out var del))
+        {
+            if (del is Action<T> action)
+                action.Invoke(data);
+            else
+                Debug.LogError($"Type mismatch for {type}");
+        }
+    }
+    #endregion
+}
